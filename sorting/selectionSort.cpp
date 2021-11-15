fa1edfa453939103a7a5102fee6d747a8bee5f1d
harshhx <hg070401@gmail.com>
2021-11-16 00:41:13 +0530

    selection sort

--- sorting/selectionSort.cpp
+++ sorting/selectionSort.cpp
@@ -14,10 +14,8 @@ void selectionSort(int arr[], int n)
     {
         int min = i;
         for (int j = i + 1; j < n; j++)
-        {
             if (arr[j] < arr[min])
                 min = j;
-        }
         if (min != i)
             swap(arr, min, i);
     }
