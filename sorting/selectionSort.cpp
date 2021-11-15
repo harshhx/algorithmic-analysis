1400e292e5ba891c453fe669686eac497f992c74
harshhx <hg070401@gmail.com>
2021-11-16 00:39:47 +0530

    selection Sort

---
+++ sorting/selectionSort.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+void selectionSort(int arr[], int n)
+{
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        if (min != i)
+            swap(arr, min, i);
+    }
+}
+
+int main()
+{
+    int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+    for (auto e : arr)
+        cout << e << " ";
+    return 0;
+}
