18d7a60c496e0d7a3b52ee66eb2351c786447a44
harshhx <hg070401@gmail.com>
2021-11-16 16:27:58 +0530

    binary search

---
+++ searching/binarySearch.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int low, int high, int e)
+{
+    if (low > high)
+        return -1;
+
+    int mid = low + (high - low) / 2;
+    if (arr[mid] > e)
+        binarySearch(arr, low, mid - 1, e);
+    else if (arr[mid] < e)
+        binarySearch(arr, mid + 1, high, e);
+    else
+        return mid;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << binarySearch(arr, 0, n - 1, 10);
+}
