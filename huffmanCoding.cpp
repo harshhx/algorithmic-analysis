5fa910b686300072c455f2def2026e6d7a3c9d41
harshhx <hg070401@gmail.com>
2021-12-18 23:20:23 +0530

    huffman

--- huffmanCoding.cpp
+++ huffmanCoding.cpp
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
   
-// A Huffman tree node
+
 struct MinHeapNode {
-  
-    // One of the input characters
+
     char data;
-  
-    // Frequency of the character
     unsigned freq;
-  
-    // Left and right child
     MinHeapNode *left, *right;
   
     MinHeapNode(char data, unsigned freq)
@@ -22,9 +17,7 @@ struct MinHeapNode {
         this->freq = freq;
     }
 };
-  
-// For comparison of
-// two heap nodes (needed in min heap)
+
 struct compare {
   
     bool operator()(MinHeapNode* l, MinHeapNode* r)
@@ -33,9 +26,6 @@ struct compare {
         return (l->freq > r->freq);
     }
 };
-  
-// Prints huffman codes from
-// the root of Huffman Tree.
 void printCodes(struct MinHeapNode* root, string str)
 {
   
@@ -49,36 +39,25 @@ void printCodes(struct MinHeapNode* root, string str)
     printCodes(root->right, str + "1");
 }
   
-// The main function that builds a Huffman Tree and
-// print codes by traversing the built Huffman Tree
+
 void HuffmanCodes(char data[], int freq[], int size)
 {
     struct MinHeapNode *left, *right, *top;
   
-    // Create a min heap & inserts all characters of data[]
+    
     priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap;
   
     for (int i = 0; i < size; ++i)
         minHeap.push(new MinHeapNode(data[i], freq[i]));
   
-    // Iterate while size of heap doesn't become 1
     while (minHeap.size() != 1) {
   
-        // Extract the two minimum
-        // freq items from min heap
         left = minHeap.top();
         minHeap.pop();
   
         right = minHeap.top();
         minHeap.pop();
-  
-        // Create a new internal node with
-        // frequency equal to the sum of the
-        // two nodes frequencies. Make the
-        // two extracted node as left and right children
-        // of this new node. Add this node
-        // to the min heap '$' is a special value
-        // for internal nodes, not used
+
         top = new MinHeapNode('$', left->freq + right->freq);
   
         top->left = left;
@@ -87,12 +66,9 @@ void HuffmanCodes(char data[], int freq[], int size)
         minHeap.push(top);
     }
   
-    // Print Huffman codes using
-    // the Huffman tree built above
     printCodes(minHeap.top(), "");
 }
   
-// Driver Code
 int main()
 {
   
